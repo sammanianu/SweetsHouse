4cf2ffb79cd6415bf81942f08daa32091d78cc6f
sammanianu <sammanianu123@gmail.com>
2018-01-03 23:43:07 +0530

    Create front-end of Home Page

--- Bakery/application/views/pages/Home.php
+++ Bakery/application/views/pages/Home.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo base_url();?>/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>/css/test.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/css/test.css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
@@ -21,6 +21,7 @@
 
             <h1 style="color: #F03861">Sweets House</h1>
 
+            <!--Nav Bar-->
             <ul class="nav navbar-nav" >
                 <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
                 <li class="dropdown">
@@ -33,12 +34,214 @@
                     </ul>
                 </li>
                 <li><a href="#">Occasions</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Instructions</a></li>
             </ul>
 
+            <!--Slide Show-->
+            <div class="w3-content w3-section" style="max-width:800px;max-height: 400px">
+                <img class="mySlides" src="<?php echo base_url().'css/images/a.jpg';?>" style="width:118%;height: 90%">
+                <img class="mySlides" src="<?php echo base_url().'css/images/b.jpg';?>" style="width:118%;height: 90%">
+                <img class="mySlides" src="<?php echo base_url().'css/images/a.jpg';?>" style="width:118%;height: 90%">
+            </div>
+
+            <script>
+                var myIndex = 0;
+                carousel();
+
+                function carousel() {
+                    var i;
+                    var x = document.getElementsByClassName("mySlides");
+                    for (i = 0; i < x.length; i++) {
+                        x[i].style.display = "none";
+                    }
+                    myIndex++;
+                    if (myIndex > x.length) {myIndex = 1}
+                    x[myIndex-1].style.display = "block";
+                    setTimeout(carousel, 2000); // Change image every 2 seconds
+                }
+            </script>
+
+            <br>
+
+            <div class="topic">
+                <h2>-------------------------------New Arrivals-----------------------------</h2>
+            </div>
+
+            <br>
+            <div class="row2">
+                <div class="col-md-4">
+                    <div class="col-sm-5 col-md-offset-1 col-md-11">
+
+                    <h3>Cakes</h3>
+                        <div class="w3-content w3-section" style="max-width:200px;max-height: 200px">
+                            <div class="mySlides2">
+                                <img src="<?php echo base_url().'css/images/OrioCheese.jpg';?>" alt="Chicago"style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="mySlides2">
+                                <img src="<?php echo base_url().'css/images/SaltedCaramel.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Salted Caramel</h3>
+                                </div>
+                            </div>
+                            <div class="mySlides2">
+                                <img src="<?php echo base_url().'css/images/FreshStrawberry.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Fresh Strawberry</h3>
+                                </div>
+                            </div>
+                           <!-- <img class="mySlides2" src="<?php /*echo base_url().'css/images/a.jpg';*/?>" style="width:118%;height: 90%">
+                            <img class="mySlides2" src="<?php /*echo base_url().'css/images/b.jpg';*/?>" style="width:118%;height: 90%">
+                            <img class="mySlides2" src="<?php /*echo base_url().'css/images/a.jpg';*/?>" style="width:118%;height: 90%">-->
+                        </div>
+
+                        <script>
+                            var myIndex2 = 0;
+                            carousel2();
+
+                            function carousel2() {
+                                var i;
+                                var x = document.getElementsByClassName("mySlides2");
+                                for (i = 0; i < x.length; i++) {
+                                    x[i].style.display = "none";
+                                }
+                                myIndex2++;
+                                if (myIndex2 > x.length) {myIndex2 = 1}
+                                x[myIndex2-1].style.display = "block";
+                                setTimeout(carousel2, 2000); // Change image every 2 seconds
+                            }
+                        </script>
+                        <br>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="col-sm-5 col-md-offset-1 col-md-11">
+                        <h3>Cup Cakes</h3>
+                        <div class="w3-content w3-section" style="max-width:200px;max-height: 200px">
+                            <div class="mySlides3">
+                                <img src="<?php echo base_url().'css/images/HoliCoffee.png';?>" alt="Chicago"style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Holi Coffee</h3>
+                                </div>
+                            </div>
+                            <div class="mySlides3">
+                                <img src="<?php echo base_url().'css/images/PeanutButter.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Peanut Butter</h3>
+                                </div>
+                            </div>
+                            <div class="mySlides3">
+                                <img src="<?php echo base_url().'css/images/BananaSplit.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                        </div>
+
+                        <script>
+                            var myIndex3 = 0;
+                            carousel3();
+
+                            function carousel3() {
+                                var i;
+                                var x = document.getElementsByClassName("mySlides3");
+                                for (i = 0; i < x.length; i++) {
+                                    x[i].style.display = "none";
+                                }
+                                myIndex3++;
+                                if (myIndex3 > x.length) {myIndex3 = 1}
+                                x[myIndex3-1].style.display = "block";
+                                setTimeout(carousel3, 2000); // Change image every 2 seconds
+                            }
+                        </script>
+                        <br>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="col-sm-5 col-md-offset-1 col-md-11">
+                        <h3>Pastries</h3>
+                        <div class="w3-content w3-section" style="max-width:200px;max-height: 200px">
+                            <div class="mySlides4">
+                                <img src="<?php echo base_url().'css/images/CheeseDanish.jpg';?>" alt="Chicago"style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Cheese Danish</h3>
+                                </div>
+                            </div>
+                            <div class="mySlides4">
+                                <img src="<?php echo base_url().'css/images/Comer.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>Comer</h3>
+                                </div>
+                            </div>
+                            <div class="mySlides4">
+                                <img src="<?php echo base_url().'css/images/DanishPastry.jpg';?>" alt="Chicago" style="width:118%;height: 90%">
+                                <div class="carousel-caption">
+                                    <h3>DanishPastry</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <script>
+                            var myIndex4 = 0;
+                            carousel4();
+
+                            function carousel4() {
+                                var i;
+                                var x = document.getElementsByClassName("mySlides4");
+                                for (i = 0; i < x.length; i++) {
+                                    x[i].style.display = "none";
+                                }
+                                myIndex4++;
+                                if (myIndex4 > x.length) {myIndex4 = 1}
+                                x[myIndex4-1].style.display = "block";
+                                setTimeout(carousel4, 2000); // Change image every 2 seconds
+                            }
+                        </script>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-5 col-md-12"  >
+                <div class="topic">
+                    <h2>----------------------------------Offers-------------------------------</h2>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <br><br>
+                    <div class="col-md-offset-1 col-md-10" style="background-color: #F5D97E">
+                        <h2>Location</h2>
+                        <h3>
+                            No:89,<br>
+                            Galle Road,<br>
+                            Matara.<br>
+                        </h3>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <br><br>
+                    <div class="col-md-offset-1 col-md-10" style="background-color: #F5D97E">
+                        <h2>Hours</h2>
+                        <h3>
+                            Week Days:9.00-6.00<br>
+                            Saturday:9.00-8.00<br>
+                            Sunday:Closed<br>
+                        </h3>
+                    </div>
+                </div>
+            </div>
 
 
-<!--<br><br><br><br><br><br><br><br><br>
--->        </div>
+
+            <br><br><br><br><br><br><br><br><br>
+
+        </div>
     </div>
 </div>
 
