3f2679db5548288658c69e31d3eec1af5e1ea183
sammanianu <sammanianu123@gmail.com>
2018-01-03 16:08:45 +0530

    Start Project

---
+++ Bakery/application/views/pages/Home.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/css/test.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<div class="container" >
+    <div class="row">
+        <div class="col-md-offset-1 col-md-10">
+
+            <h1 style="color: #F03861">Sweets House</h1>
+
+            <ul class="nav navbar-nav" >
+                <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Cakes</a></li>
+                        <li><a href="#">Cup Cakes</a></li>
+                        <li><a href="#">Pastries</a></li>
+                        <li><a href="#">Breads</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Occasions</a></li>
+            </ul>
+
+
+
+<!--<br><br><br><br><br><br><br><br><br>
+-->        </div>
+    </div>
+</div>
+
+
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+-->
+</body>
+
+</html>
